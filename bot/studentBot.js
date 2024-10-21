567f00848a878af64edee7fc5864ddfabf598c52
Yabe12 <yeabsirabehailu92@gmail.com>
2024-10-21 10:11:52 +0300

    npm install dotenv

---
+++ bot/studentBot.js
@@ -0,0 +1,46 @@
+const { Telegraf, Markup } = require('telegraf');
+
+
+const bot = new Telegraf('');
+
+
+
+
+
+
+bot.start((ctx) => {
+    ctx.reply('Welcome! Type "Register" to begin the registration process.');
+});
+
+
+bot.command('register', (ctx) => {
+    ctx.reply('Click the button below to register:',
+        Markup.inlineKeyboard([
+            Markup.button.webApp('Register', 'https://miniapp-ten-zeta.vercel.app/') 
+        ])
+    );
+});
+
+bot.on('text', (ctx) => {
+    if (ctx.message.text.toLowerCase() === 'register') {
+     
+        ctx.reply('Click the button below to register:',
+            Markup.inlineKeyboard([
+                Markup.button.webApp('Register', 'https://miniapp-ten-zeta.vercel.app/')
+            ])
+        );
+    }
+});
+
+bot.catch((err) => {
+    console.error('Error in bot:', err);
+});
+
+
+bot.launch()
+    .then(() => {
+        console.log('Bot is running...');
+    })
+    .catch(err => {
+        console.error('Failed to launch bot:', err);
+    });
