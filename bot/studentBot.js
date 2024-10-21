abbc11441f43341ab6273eee060660164b3c0985
Yabe12 <yeabsirabehailu92@gmail.com>
2024-10-21 10:13:30 +0300

    npm install telegraf

--- bot/studentBot.js
+++ bot/studentBot.js
@@ -1,29 +1,22 @@
+require('dotenv').config(); // Load environment variables from .env file
 const { Telegraf, Markup } = require('telegraf');
 
-
-const bot = new Telegraf('');
-
-
-
-
-
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN); // Use the bot token from the .env file
 
 bot.start((ctx) => {
     ctx.reply('Welcome! Type "Register" to begin the registration process.');
 });
 
-
 bot.command('register', (ctx) => {
     ctx.reply('Click the button below to register:',
         Markup.inlineKeyboard([
-            Markup.button.webApp('Register', 'https://miniapp-ten-zeta.vercel.app/') 
+            Markup.button.webApp('Register', 'https://miniapp-ten-zeta.vercel.app/')
         ])
     );
 });
 
 bot.on('text', (ctx) => {
     if (ctx.message.text.toLowerCase() === 'register') {
-     
         ctx.reply('Click the button below to register:',
             Markup.inlineKeyboard([
                 Markup.button.webApp('Register', 'https://miniapp-ten-zeta.vercel.app/')
@@ -35,7 +28,6 @@ bot.on('text', (ctx) => {
 bot.catch((err) => {
     console.error('Error in bot:', err);
 });
-
 
 bot.launch()
     .then(() => {
